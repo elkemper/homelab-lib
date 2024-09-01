a8c9a09e6020120c485791d3faf8bc5c9b61f763
Vladislav Belous <elkemper@users.noreply.github.com>
2024-09-01 22:44:45 +0300

    add dotenv and fix config a bit

--- config.ts
+++ config.ts
@@ -1,14 +1,16 @@
+import dotenv from 'dotenv'
+
+dotenv.config()
 export default {
   port: parseInt(process.env.PORT) || 3214,
-  dbPath: process.env.DB_PATH || '',
-  archivePath: process.env.ARCHIVE_PATH || '',
+  dbPath: process.env.DB_PATH,
+  archivePath: process.env.ARCHIVE_PATH,
   defaultPerPage: 50,
-  adminUsername: process.env.ADMIN_USERNAME || '',
-  adminPassword: process.env.ADMIN_PASSWORD || '',
+  adminUsername: process.env.ADMIN_USERNAME,
+  adminPassword: process.env.ADMIN_PASSWORD,
   salt: 10,
   sessionCheckingTimeout: 24 * 60 * 60 * 1000,
   jwtExpiration: '1d',
-  jwtSecret: process.env.TOKEN_SECRET || '',
-  maxRequestsIn10sec: parseInt(process.env.REQUEST_RATE_LIMIT) || 20,
-  backendUnderSlashApi: process.env.SLASH_API && process.env.SLASH_API .toLowerCase() === 'true'
+  jwtSecret: process.env.TOKEN_SECRET,
+  maxRequestsPer10sec: parseInt(process.env.REQUEST_RATE_LIMIT) || 20,
 };
